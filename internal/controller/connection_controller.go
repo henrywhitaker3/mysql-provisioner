efb86d5b52f9e51beb7971b8ec276291dc674d88
Henry Whitaker <henrywhitaker3@outlook.com>
2023-06-11 15:44:06 +0100

    add sample crd to samples

--- internal/controller/connection_controller.go
+++ internal/controller/connection_controller.go
@@ -18,6 +18,7 @@ package controller
 
 import (
 	"context"
+	"fmt"
 
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
@@ -39,17 +40,25 @@ type ConnectionReconciler struct {
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
-// TODO(user): Modify the Reconcile function to compare the state specified by
-// the Connection object against the actual cluster state, and then
-// perform operations to make the cluster state reflect the state specified by
-// the user.
 //
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.14.4/pkg/reconcile
 func (r *ConnectionReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
+	l := log.FromContext(ctx)
 
-	// TODO(user): your logic here
+	d := &mysqlprovisionerv1beta1.Connection{}
+	if err := r.Get(ctx, req.NamespacedName, d); err != nil {
+		return ctrl.Result{}, client.IgnoreNotFound(err)
+	}
+
+	// Check if the object is being deleted
+	if !d.ObjectMeta.DeletionTimestamp.IsZero() {
+		l.Info("mysql-provisioner.henrywhitaker.com/connection being deleted")
+	}
+
+	fmt.Println(d)
+
+	// Handle creating whatever resource we need to
 
 	return ctrl.Result{}, nil
 }
